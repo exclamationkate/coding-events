c5aa90cc18be7537df5d7b35f60bfea65d491140
Kate Steib <kate.steib@gmail.com>
2020-01-27 00:44:09 -0600

    Chapter 18.3 reading complete. Added one-to-many EventCategory to Event relationship.

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,7 +1,10 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -11,6 +14,9 @@ public class EventCategory extends AbstractEntity {
 
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory(String name) {
         this.name = name;
@@ -23,6 +29,8 @@ public class EventCategory extends AbstractEntity {
     }
 
     public void setName(String name) { this.name = name; }
+
+    public List<Event> getEvents() { return events; }
 
     @Override
     public String toString() { return name; }
